5a4c24fd7e4493a713f30698a972e7a45bb67c71
Hello9999901 <byran8855@gmail.com>
2022-12-24 11:01:50 -0500

    feat(buoyancymodule): added fully-fleged frontend

--- BuoyancyModule/FrontEnd/src/routes/+page.svelte
+++ BuoyancyModule/FrontEnd/src/routes/+page.svelte
@@ -1,18 +1,44 @@
-<button id="read">Connect with BLE device</button>
-<button id="start" disabled>Start</button>
-<button id="stop" disabled>Stop</button>
-<button id="writeCurrentTime">Update to Current Time</button>
-<button id="write">Write Custom Text</button>
-<input id="input" placeholder="custom text input"></input>
+<div class="absolute flex flex-col w-screen py-10 items-center justify-center font-medium text-3xl text-neutral-200">
+  <h1>MUREX ROV Buoyancy Module Topside Control</h1>
+  <h1 class="text-lg font-normal">ESP32 with BLE GATT, Custom 128-bit UUID Service</h1>
+  <h1 class="text-lg font-normal">Chrome 58+, Bluetooth Web API</h1>
+</div>
 
-<script>
+<div class="absolute flex w-screen h-2/3 items-center justify-center">
+  <button id="read">Connect BLE Device</button>
+  <button id="start" disabled>Start Reporting</button>
+  <button id="stop" disabled>Stop Reporting</button>
+  <button id="write">Write Custom Text</button>
+  <input id="input" placeholder="custom text input"/>
+  <button id="writeCurrentTime">Update to Current Time</button>
+</div>
+
+<div class="flex w-screen h-screen items-center justify-center font-bold text-4xl text-neutral-200">
+  <h1>{output}</h1>
+</div>
+
+<script lang="js">
+	import { onMount } from 'svelte';
+	import "../app.css";
+  // Change name based on ESP32 device name, will filter to only matching name device
   var deviceName = 'ESP32 Byran'
   var bleService = '1aade6e4-c95a-419f-bcd7-6ab375f19e01'
   var bleCharacteristic = 'fcfcad0b-035c-49e7-a828-ef109bce0682'
+  /**
+	 * @type {{ gatt: { connected: any; connect: () => Promise<any>; }; }}
+	 */
   var bluetoothDeviceDetected
+  /**
+	 * @type {{ writeValue: (arg0: Uint8Array) => any; readValue: () => any; addEventListener: (arg0: string, arg1: (event: any) => void) => void; startNotifications: () => Promise<any>; stopNotifications: () => Promise<any>; }}
+	 */
   var gattCharacteristic
+	/**
+	 * @type {string}
+	 */
+	var output = "No Bluetooth Device Connected..."
 
-  document.querySelector('#read').addEventListener('click', function() {
+	onMount(() => {
+		document.querySelector('#read').addEventListener('click', function() {
     if (isWebBluetoothEnabled()) { read() }
   })
 
@@ -31,6 +57,7 @@
   document.querySelector('#writeCurrentTime').addEventListener('click', function(event) {
     if (isWebBluetoothEnabled()) { write(new Date(Date.now()).getUTCHours() + " " + new Date(Date.now()).getUTCMinutes() + " " + new Date(Date.now()).getUTCSeconds() + " ") }
   })
+	});
 
   function isWebBluetoothEnabled() {
     if (!navigator.bluetooth) {
@@ -110,6 +137,7 @@
   function handleChangedValue(event) {
     let value = event.target.value
     var now = new Date()
+		$: output = new TextDecoder("utf-8").decode(value)
     console.log('> ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + ' Value is ' + new TextDecoder("utf-8").decode(value))
   }
 
