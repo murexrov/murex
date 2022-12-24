b208788d1446d31626949f4a7f25faa6d433f60a
Hello9999901 <byran8855@gmail.com>
2022-12-23 23:32:48 -0500

    buoyancy module code complete, needs front-end work

--- BuoyancyModule/BLE_Gatt.ino
+++ BuoyancyModule/BLE_Gatt.ino
@@ -8,14 +8,23 @@
 
 #include <ESP32Time.h>
 
+// Instantiates ESP32 built in RTC
+
 ESP32Time rtc(0);
 
 bool _BLEClientConnected = false;
 
+std::string temp = "";
+
+int teamNumber = 999999;
+
+// Instantiates generic custom uuid
 #define MetaDataService "1aade6e4-c95a-419f-bcd7-6ab375f19e01"
 
+// This characteristic can be written to, read from and report notifications
 BLECharacteristic MetaDataCharacteristic("fcfcad0b-035c-49e7-a828-ef109bce0682", BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
 
+// BLE Server for notifications
 BLEDescriptor MetaDataDescriptor(BLEUUID((uint16_t)0x2901));
 
 class MyServerCallbacks : public BLEServerCallbacks {
@@ -63,17 +72,50 @@ void InitBLE() {
 }
 
 void setup() {
+  // Sets baud rate to 115200
   Serial.begin(115200);
+
   Serial.println("UTC and Team Number Indicator - BLE");
+
+  // Built-in RTC random value set
   rtc.setTime(59, 26, 13, 23, 12, 2022);
+
+  // Initializes the BLE server
   InitBLE();
 }
 
 void loop() {
-  // Serial.println(rtc.getTime("%H:%M:%S"));
-  Serial.println(MetaDataCharacteristic.getValue().c_str());
-  std::string value = ((rtc.getTime("%H:%M:%S") + " 999999").c_str());
+  if (MetaDataCharacteristic.getValue().c_str() != temp) {
+    // Serial.println("Input Updated:");
+    int timeArray[3], r = 0, t = 0;
+
+    String inputFromComputer = MetaDataCharacteristic.getValue().c_str();
+
+    // Splits string into int array
+    for (int i = 0; i < inputFromComputer.length(); i++) {
+      if (inputFromComputer.charAt(i) == ' ') {
+        timeArray[t] = inputFromComputer.substring(r, i).toInt();
+        r = (i + 1);
+        t++;
+      }
+    }
+    // Serial.println(String(timeArray[0]) + " " + String(timeArray[1]) + " " + String(timeArray[2]));
+
+    // including manual offset of 3 from testing, should be changed depending on circumstances
+    int offset = 2;
+
+    // updates rtc time based on input from connected computer
+    rtc.setTime(timeArray[2] + offset, timeArray[1], timeArray[0], 1, 1, 2023);
+  }
+
+  // Serial.println(MetaDataCharacteristic.getValue().c_str());
+  std::string value = ((rtc.getTime("%H:%M:%S") + " Team Number: " + String(teamNumber)).c_str());
+
+  temp = value;
+
   MetaDataCharacteristic.setValue(value);
   MetaDataCharacteristic.notify();
+
+  // update every second
   delay(1000);
 }
