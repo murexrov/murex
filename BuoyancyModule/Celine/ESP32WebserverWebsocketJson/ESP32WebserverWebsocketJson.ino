a4c8620102ad86f77d8150efe7cf32f3a5aceac4
chzhang2007 <celine.zhang2007@gmail.com>
2023-01-03 15:08:27 -0700

    More bug fixes

--- BuoyancyModule/Celine/ESP32WebserverWebsocketJson/ESP32WebserverWebsocketJson.ino
+++ BuoyancyModule/Celine/ESP32WebserverWebsocketJson/ESP32WebserverWebsocketJson.ino
@@ -19,10 +19,6 @@ IPAddress subnet(255,255,255,0);
 // The String below "webpage" contains the complete HTML code that is sent to the client whenever someone connects to the webserver
 String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Time</h1><p>Time: <span id='time'>-</span></p><p><button type='button' id='BTN_SEND_BACK'>Send info to ESP32</button></p></span></body><script> var Socket; document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back); function init() { Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) { processCommand(event); }; } function button_send_back() { var msg = {time: new Date()};Socket.send(JSON.stringify(msg)); } function processCommand(event) {var obj = JSON.parse(event.data);document.getElementById('time').innerHTML = obj.time; console.log(obj.time);} window.onload = function(event) { init(); }</script></html>";
 
-// We want to periodically send values to the clients, so we need to define an "interval" and remember the last time we sent data to the client (with "previousMillis")
-int interval = 1000;                                  // send data to the client every 1000ms -> 1s
-unsigned long previousMillis = 0;                     // we use the "millis()" command for time reference and this will output an unsigned long
-
 // Initialization of webserver and websocket
 WebServer server(80);                                 // the server uses port 80 (standard port for websites
 WebSocketsServer webSocket = WebSocketsServer(81);    // the websocket uses port 81 (standard port for websockets
@@ -53,31 +49,25 @@ void setup() {
 void loop() {
   server.handleClient();                              // Needed for the webserver to handle all clients
   webSocket.loop();                                   // Update function for the webSockets
-
-  unsigned long now = millis();                       // read out the current "time" ("millis()" gives the time in ms since the Arduino started)
-  if ((unsigned long)(now - previousMillis) > interval) { // check if "interval" ms has passed since last time the clients were updated
-
-    String jsonString = "";                           // create a JSON string for sending data to the client
-    StaticJsonDocument<200> doc;                      // create a JSON container
-    JsonObject object = doc.to<JsonObject>();         // create a JSON Object
-    int timeArray[3], r = 0, t = 0;
-    String time = ((rtc.getTime("%H:%M:%S")).c_str());
-    for (int i = 0; i < time.length(); i++) {
-      if (time.charAt(i) == ' ') {
-        timeArray[t] = time.substring(r, i).toInt();
-        r = (i + 1);
-        t++;
-      }
+  String jsonString = "";                           // create a JSON string for sending data to the client
+  StaticJsonDocument<200> doc;                      // create a JSON container
+  JsonObject object = doc.to<JsonObject>();         // create a JSON Object
+  int timeArray[3], r = 0, t = 0;
+  String time = (rtc.getTime("%H:%M:%S");
+  for (int i = 0; i < time.length(); i++) {
+    if (time.charAt(i) == ' ') {
+      timeArray[t] = time.substring(r, i).toInt();
+      r = (i + 1);
+      t++;
     }
-    int offset = 2;
-    rtc.setTime(timeArray[2] + offset, timeArray[1], timeArray[0], 1, 1, 2023);
-    object["time"] = time;
-    serializeJson(doc, jsonString);                   // convert JSON object to string
-    Serial.println(jsonString);                       // print JSON string to console for debug purposes
-    webSocket.broadcastTXT(jsonString);               // send JSON string to clients
-
-    previousMillis = now;                             // reset previousMillis
   }
+  int offset = 2;
+  rtc.setTime(timeArray[2] + offset, timeArray[1], timeArray[0], 1, 1, 2023);
+  object["time"] = time;
+  serializeJson(doc, jsonString);                   // convert JSON object to string
+  Serial.println(jsonString);                       // print JSON string to console for debug purposes
+  webSocket.broadcastTXT(jsonString);               // send JSON string to clients
+  delay(1000);
 }
 
 void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
