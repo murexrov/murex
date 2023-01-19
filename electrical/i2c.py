033fe16922ccf0f5e9a14cf32c2bde4fd8395698
xamuil2 <61640888+xamuil2@users.noreply.github.com>
2023-01-18 21:54:40 -0500

    implemented i2c for all devices
    Confirmed devices so far:
    - PCA9685 Driver
    - BNO055 IMU
    - BME680 Sensor

--- electrical/i2c.py
+++ electrical/i2c.py
@@ -6,6 +6,12 @@
 # 2. BNO055 IMU: https://github.com/adafruit/Adafruit_CircuitPython_BNO055
 # 3. BME680 Sensor: https://github.com/adafruit/Adafruit_CircuitPython_BME680
 
+# run these commands after installations:
+# sudo pip3 install adafruit-circuitpython-pca9685
+# sudo pip3 install adafruit-circuitpython-servokit
+# sudo pip3 install adafruit-circuitpython-bno055
+# sudo pip3 install adafruit-circuitpython-bme680
+
 import time
 import board
 from board import SCL, SDA
@@ -16,19 +22,19 @@ from adafruit_pca9685 import PCA9685
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 import adafruit_motor.servo
-
 import adafruit_bno055
 import adafruit_bme680
 
-#PCA9685 Driver
+# PCA9685 Driver
 i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
+# Example: stop signals for all thrusters connected to pwm channels
 for i in range(16):
-    kit.servo[i].set_pulse_width(1500)
+    kit.servo[i].set_pulse_width(1500) # "i" refers to the channel on PCA9685
 
-#BNO055 IMU
+# BNO055 IMU
 i2c = board.I2C(SCL, SDA)
 bno055 = adafruit_bno055.BNO055_I2C(i2c)
 
@@ -44,6 +50,7 @@ def temperature():
     last_val = result
     return result
 
+# example access of sensor values + units
 print("Temperature: {} degrees C".format(bno055.temperature))
 print("Accelerometer (m/s^2): {}".format(bno055.acceleration))
 print("Magnetometer (microteslas): {}".format(bno055.magnetic))
@@ -62,6 +69,7 @@ bme680.sea_level_pressure = 1013.25
 #this temperature sensor is kinda goofy so it's good we have 2 temp sensors that we can use to cross check and calibrate
 temperature_offset = -5
 
+# example access of sensor values + units
 print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
 print("Gas: %d ohm" % bme680.gas)
 print("Humidity: %0.1f %%" % bme680.relative_humidity)
