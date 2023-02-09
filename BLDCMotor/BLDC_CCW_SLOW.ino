ce5f784a18202a56443f2f848bcb029d13f119ec
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2023-02-08 19:47:53 -0500

    added bldc tests without input

---
+++ BLDCMotor/BLDC_CCW_SLOW.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+#define CW_CWW_PIN 10
+#define PWM_PIN 11
+
+void setup() {
+  pinMode(CW_CWW_PIN, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(CW_CWW_PIN, LOW);
+  analogWrite(PWM_PIN, 200);
+}
