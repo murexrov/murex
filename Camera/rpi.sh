02b0af2e5f1a9078d128fa4716adfc4ba1f80caa
chzhang2007 <celine.zhang2007@gmail.com>
2023-02-25 08:32:19 -0500

    Improve webcam performance
    Added parameters to line receiving on topside computer, this decreased delay by a lot

--- Camera/rpi.sh
+++ Camera/rpi.sh
@@ -1,4 +1,4 @@
 #stream from webcam
-ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -c:a copy -analyzeduration 0 -f mpegts udp://192.168.100.52:1234
+ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -b:v 125000 -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -analyzeduration 0 -f mpegts udp://192.168.100.52:1234
 #receive on topside computer
-ffplay udp://192.168.100.1:1234
+ffplay -fflags nobuffer -flags low_delay -framedrop -strict experimental udp://192.168.100.1:1234
